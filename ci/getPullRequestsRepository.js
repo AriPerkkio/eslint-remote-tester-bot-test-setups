dede7965fb849c2b9954caee7a7bb06c835a568f
Ari Perkkio <ari.perkkio@gmail.com>
2021-01-17 20:13:35 +0200

    wip

---
+++ ci/getPullRequestsRepository.js
@@ -0,0 +1,36 @@
+const core = require("@actions/core");
+const exec = require('@actions/exec');
+
+const { context, getOctokit } = require("@actions/github");
+
+const TRIGGER_KEYWORD = "@eslint-remote-tester-bot";
+
+async function run() {
+  try {
+    const octokit = getOctokit(process.env.GITHUB_TOKEN);
+
+    if (context.eventName !== "issue_comment") {
+      return console.warn(`invalid eventName: (${context.eventName})`);
+    }
+
+    const comment = context.payload.comment.body;
+    if (!comment.startsWith(TRIGGER_KEYWORD)) return;
+
+    const pullRequest = await octokit.pulls.get({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: context.issue.number,
+    });
+
+    const output = {
+      repository: pullRequest.data.head.repo.full_name,
+      branch: pullRequest.data.head.ref,
+    };
+
+    return output;
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
--- ci/index.js
+++ ci/index.js
@@ -7,28 +7,7 @@ const TRIGGER_KEYWORD = "@eslint-remote-tester-bot";
 
 async function run() {
   try {
-    const octokit = getOctokit(process.env.GITHUB_TOKEN);
-
-    if (context.eventName !== "issue_comment") {
-      return console.warn(`invalid eventName: (${context.eventName})`);
-    }
-
-    const comment = context.payload.comment.body;
-    if (!comment.startsWith(TRIGGER_KEYWORD)) return;
-
-    const pullRequest = await octokit.pulls.get({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      pull_number: context.issue.number,
-    });
-
-    const prRepository = pullRequest.data.head.repo.full_name;
-    const prBranch = pullRequest.data.head.ref;
-
-    core.info(`prRepository ${prRepository} prBranch ${prBranch}`);
-
-    await exec.exec('./node_modules/.bin/eslint-remote-tester');
-
+    await exec.exec('./node_modules/.bin/eslint-remote-tester', [], { failOnStdErr: false });
   } catch (error) {
     core.setFailed(error.message);
   }
