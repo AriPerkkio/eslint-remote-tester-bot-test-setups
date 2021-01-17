ae881b17a2e31f47ad0e0c9afb2d148cb4a470e3
Ari Perkkio <ari.perkkio@gmail.com>
2021-01-17 20:02:03 +0200

    wip

--- ci/index.js
+++ ci/index.js
@@ -1,11 +1,13 @@
 const core = require("@actions/core");
+const exec = require('@actions/exec');
+
 const { context, getOctokit } = require("@actions/github");
 
 const TRIGGER_KEYWORD = "@eslint-remote-tester-bot";
 
 async function run() {
   try {
-    const client = getOctokit(process.env.GITHUB_TOKEN);
+    const octokit = getOctokit(process.env.GITHUB_TOKEN);
 
     if (context.eventName !== "issue_comment") {
       return console.warn(`invalid eventName: (${context.eventName})`);
@@ -14,7 +16,7 @@ async function run() {
     const comment = context.payload.comment.body;
     if (!comment.startsWith(TRIGGER_KEYWORD)) return;
 
-    const pullRequest = await client.pulls.get({
+    const pullRequest = await octokit.pulls.get({
       owner: context.repo.owner,
       repo: context.repo.repo,
       pull_number: context.issue.number,
@@ -24,6 +26,8 @@ async function run() {
     const prBranch = pullRequest.data.head.ref;
 
     core.info(`prRepository ${prRepository} prBranch ${prBranch}`);
+
+    await exec.exec('./node_modules/.bin/eslint-remote-tester');
 
   } catch (error) {
     core.setFailed(error.message);
