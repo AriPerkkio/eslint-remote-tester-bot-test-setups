0b807b13a06013b86972b75c493256eeb777a29d
Ari Perkkio <ari.perkkio@gmail.com>
2021-02-14 10:37:16 +0200

    wip

---
+++ eslint-remote-tester.config smoke.js
@@ -0,0 +1,22 @@
+/** @type {import('eslint-remote-tester/dist/config/types').Config} */
+module.exports = {
+    repositories: ['AriPerkkio/eslint-remote-tester-integration-test-target'],
+    extensions: ['.js'],
+    rulesUnderTesting: ['local-rules/no-foo'],
+    eslintrc: {
+        root: true,
+        plugins: ['eslint-plugin-local-rules'],
+        rules: {
+            'local-rules/no-foo': 'error',
+        },
+    },
+    onComplete: async function onComplete(results, comparisonResults) {
+        console.log("Smoke test onComplete");
+        console.log(`${results.length} results`);
+        console.log(`${Boolean(comparisonResults)} comparisonResults`);
+
+        console.log('Sleeping 5s');
+        await new Promise(r => setTimeout(r, 5000));
+        console.log('Done');
+    }
+};
