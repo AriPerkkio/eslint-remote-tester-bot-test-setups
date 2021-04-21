6c1b36246a32b0a8320c09c05acb052f95bf2462
Ari Perkkio <ari.perkkio@gmail.com>
2021-04-21 17:51:04 +0300

    wip

--- eslint-remote-tester.config.js
+++ eslint-remote-tester.config.js
@@ -1,15 +1,20 @@
+const { getRepositories } = require("eslint-remote-tester-repositories");
+
 /** @type {import('eslint-remote-tester/dist/config/types').Config} */
 module.exports = {
-    repositories: ['AriPerkkio/eslint-remote-tester-integration-test-target'],
-    extensions: ['.js'],
-    rulesUnderTesting: ['local-rules/no-foo'],
-    compare: true,
-    CI: false,
-    eslintrc: {
-        root: true,
-        plugins: ['eslint-plugin-local-rules'],
-        rules: {
-            'local-rules/no-foo': 'error',
-        },
+  repositories: [
+    "AriPerkkio/eslint-remote-tester-integration-test-target",
+    getRepositories({ randomize: true }).slice(0, 2),
+  ],
+  extensions: [".js"],
+  rulesUnderTesting: ["local-rules/no-foo"],
+  compare: true,
+  CI: false,
+  eslintrc: {
+    root: true,
+    plugins: ["eslint-plugin-local-rules"],
+    rules: {
+      "local-rules/no-foo": "error",
     },
+  },
 };
