3ffdedfec26b985bfb85d3b64b51e7582835c152
Ari Perkkio <ari.perkkio@gmail.com>
2021-05-21 17:48:04 +0300

    wip

--- eslint-remote-tester.config.smoke.js
+++ eslint-remote-tester.config.smoke.js
@@ -1,13 +1,15 @@
+console.log("Smoke test config");
+
 /** @type {import('eslint-remote-tester/dist/config/types').Config} */
 module.exports = {
-    repositories: ['AriPerkkio/eslint-remote-tester-integration-test-target'],
-    extensions: ['.js'],
-    rulesUnderTesting: ['local-rules/no-foo'],
+    repositories: ["AriPerkkio/eslint-remote-tester-integration-test-target"],
+    extensions: [".js"],
+    rulesUnderTesting: ["local-rules/no-foo"],
     eslintrc: {
         root: true,
-        plugins: ['eslint-plugin-local-rules'],
+        plugins: ["eslint-plugin-local-rules"],
         rules: {
-            'local-rules/no-foo': 'error',
+            "local-rules/no-foo": "error",
         },
     },
     onComplete: async function onComplete(results, comparisonResults) {
@@ -15,8 +17,8 @@ module.exports = {
         console.log(`${results.length} results`);
         console.log(`${Boolean(comparisonResults)} comparisonResults`);
 
-        console.log('Sleeping 5s');
-        await new Promise(r => setTimeout(r, 5000));
-        console.log('Done');
-    }
+        console.log("Sleeping 5s");
+        await new Promise((r) => setTimeout(r, 5000));
+        console.log("Done");
+    },
 };
